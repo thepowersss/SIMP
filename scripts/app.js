59018264aabad9d4946cffb830899649ebd8e537
Sherwin Zhang <sherwinzhang100@gmail.com>
2020-11-28 19:01:10 -0800

    server test2
    with mysql and http

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require('http'); //what web framework?
 
 const hostname = '140.82.49.189';
 const port = 8080;
@@ -12,3 +12,29 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/* talk to mySQL here */
+var mysql = require('mysql');
+
+// read from password.txt, load into environment variable
+
+var con = mysql.createConnection({
+  host: "140.82.49.189", //read documentation for host, need port?
+  user: "guestuser",
+  password: "password", // remember to message james
+  database: "simp_notes"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+}
+
+con.query('SELECT * FROM notes', function(error, result, fields) {
+    if (error) throw error;
+    console.log("The solution is: ", result);
+});
+
+con.end();
