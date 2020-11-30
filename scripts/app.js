307876433d138b27baefafa17c3f51346433b96b
Sherwin Zhang <sherwinzhang100@gmail.com>
2020-11-29 23:30:11 -0800

    linted

--- scripts/app.js
+++ scripts/app.js
@@ -10,31 +10,10 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 /* talk to mySQL here */
 var mysql = require('mysql');
 
 // read from password.txt, load into environment variable
-
-var con = mysql.createConnection({
-  host: "140.82.49.189", //read documentation for host, need port?
-  user: "guestuser",
-  password: "password", // remember to message james
-  database: "simp_notes"
-});
-
-con.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-});
-
-con.query('SELECT * FROM notes', function(error, result, fields) {
-    if (error) throw error;
-    console.log("The solution is: ", result);
-});
-
-con.end();
--- scripts/db_connection.js
+++ scripts/db_connection.js
@@ -1,21 +1,25 @@
-/*var mysql = require('mysql');
+/* talk to mySQL here */
+var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "140.82.49.189",
-  user: "guestuser",
-  password: "password",
-  database: "simp_notes"
+    host: "140.82.49.189", //read documentation for host, need port?
+    user: "guestuser",
+    password: "password", // read from password.txt, load into environment variable?
+    database: "simp_notes"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to jame server!");
+con.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
 });
 
 con.query('SELECT * FROM notes', function(error, result, fields) {
-    if (error) throw error;
+    if (error) {
+    	throw error;
+    }
     console.log("The solution is: ", result);
 });
 
 con.end();
-*/
