46210dc8562afe17ad82776cd7ab30ee8d26bf21
Sherwin Zhang <sherwinzhang100@gmail.com>
2020-11-30 01:49:35 -0800

    using express.static

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/',function(req,res){
-  res.sendFile('/root/sherwin_SIMP/simp.html');
-  res.sendFile('/root/sherwin_SIMP/scripts/main.js');
-  res.sendFile('/root/sherwin_SIMP/style.css');
+app.use(express.static("../public"));
+
+/*app.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
   // ****read
 });
-
+*/
 console.log('Running at Port 3000');
 
 app.post('/save', function(req, res) { 
