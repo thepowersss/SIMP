6b13e569bce0509d4aec7ed00725b6dec45f409c
Sherwin Zhang <sherwinzhang100@gmail.com>
2020-11-30 01:33:27 -0800

    removed router

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = express.Router();
 
-router.get('/',function(req,res){
+app.get('/',function(req,res){
   res.sendFile('/root/sherwin_SIMP/simp.html');
   //__dirname : It will resolve to your project folder.
+  // ****read
+  // serve
 });
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
 console.log('Running at Port 3000');
+
+app.post('/save', function(req, res) { 
+  // ****write
+  // when the browser posts onto /save
+  // create/update/delete row with buttons
+  // do stuff with res after mysql.connection 
+});
+
+app.listen(process.env.port || 3000);
